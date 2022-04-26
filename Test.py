cc8be94019803c081779cb8e866f05664de18b24
vishal20156 <vishalramnani171@gmail.com>
2022-04-26 15:33:56 +0530

    Jenkins Python

---
+++ Test.py
@@ -0,0 +1,11 @@
+import sys
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+
+print("Addition is :", a+b)
+print("Subtraction is:",a-b)
+print("Multiplication is: ",a*b)
+print("Division is: ",a//b)
+
+
